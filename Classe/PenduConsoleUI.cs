558e42ffe0994191c506c70e69addd659368a348
tmnpierre <tomyn.pierre@gmail.com>
2024-01-05 20:48:23 +0100

    feat(ui): implement display and input methods in PenduConsoleUI

--- Classe/PenduConsoleUI.cs
+++ Classe/PenduConsoleUI.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleHangman
+﻿namespace ConsoleHangman
 {
-    internal class PenduConsoleUI
+    public class PenduConsoleUI
     {
+        private Pendu _jeuPendu;
+
+        public PenduConsoleUI(Pendu jeuPendu)
+        {
+            _jeuPendu = jeuPendu;
+        }
+
+        public void AfficherMasque()
+        {
+            Console.WriteLine("Mot à deviner: " + _jeuPendu.GetMasque());
+        }
+
+        public char DemanderLettre()
+        {
+            Console.Write("Entrez une lettre: ");
+            string input = Console.ReadLine();
+            return !string.IsNullOrWhiteSpace(input) && input.Length == 1 ? input[0] : ' ';
+        }
+
+        public void AfficherResultat()
+        {
+            if (_jeuPendu.TestWin())
+            {
+                Console.WriteLine("Félicitations ! Vous avez trouvé le mot.");
+            }
+            else
+            {
+                Console.WriteLine("Désolé, vous avez perdu. Le mot était : " + _jeuPendu.GetMotATrouver());
+            }
+        }
+
+        public void AfficherNombreEssais()
+        {
+            Console.WriteLine("Nombre d'essais restants: " + _jeuPendu.GetNombreEssais());
+        }
     }
 }
