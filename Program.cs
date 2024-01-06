391cbfa4e61fee0375f5d43126dbcc5805a6b6c5
tmnpierre <tomyn.pierre@gmail.com>
2024-01-06 21:11:23 +0100

    feat(main): integrate Pendu and PenduConsoleUI in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using ConsoleHangman;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string motADeviner = "exemple";
+        Pendu jeuPendu = new Pendu(motADeviner);
+        PenduConsoleUI ui = new PenduConsoleUI(jeuPendu);
+        ui.Jouer();
+    }
+}
