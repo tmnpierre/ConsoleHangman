8b639861f3b2e66a3e52f009265f8af39ce6cfeb
tmnpierre <tomyn.pierre@gmail.com>
2024-01-06 21:19:30 +0100

    feat(ui): complete game interaction and display logic in PenduConsoleUI

--- Classe/PenduConsoleUI.cs
+++ Classe/PenduConsoleUI.cs
@@ -1,53 +1,52 @@
-﻿namespace ConsoleHangman
+﻿using System;
+
+public class PenduConsoleUI
 {
-    public class PenduConsoleUI
+    private Pendu _jeuPendu;
+
+    public PenduConsoleUI(Pendu jeuPendu)
     {
-        private Pendu _jeuPendu;
+        _jeuPendu = jeuPendu;
+    }
 
-        public PenduConsoleUI(Pendu jeuPendu)
+    public void Jouer()
+    {
+        while (!_jeuPendu.TestWin() && _jeuPendu.GetNombreEssais() > 0)
         {
-            _jeuPendu = jeuPendu;
+            AfficherMasque();
+            AfficherNombreEssais();
+            char lettre = DemanderLettre();
+            _jeuPendu.TestChar(lettre);
         }
+        AfficherResultat();
+    }
 
-        public void Jouer()
-        {
-            while (!_jeuPendu.TestWin() && _jeuPendu.GetNombreEssais() > 0)
-            {
-                AfficherMasque();
-                AfficherNombreEssais();
-                char lettre = DemanderLettre();
-                _jeuPendu.TestChar(lettre);
-            }
-            AfficherResultat();
-        }
+    public void AfficherMasque()
+    {
+        Console.WriteLine("Mot à deviner: " + _jeuPendu.GetMasque());
+    }
 
-        public void AfficherMasque()
-        {
-            Console.WriteLine("Mot à deviner: " + _jeuPendu.GetMasque());
-        }
+    public char DemanderLettre()
+    {
+        Console.Write("Entrez une lettre: ");
+        string input = Console.ReadLine();
+        return !string.IsNullOrWhiteSpace(input) && input.Length == 1 ? input[0] : ' ';
+    }
 
-        public char DemanderLettre()
+    public void AfficherResultat()
+    {
+        if (_jeuPendu.TestWin())
         {
-            Console.Write("Entrez une lettre: ");
-            string input = Console.ReadLine();
-            return !string.IsNullOrWhiteSpace(input) && input.Length == 1 ? input[0] : ' ';
+            Console.WriteLine("Félicitations ! Vous avez trouvé le mot.");
         }
+        else
+        {
+            Console.WriteLine("Désolé, vous avez perdu. Le mot était : " + _jeuPendu.GetMotATrouver());
+        }
+    }
 
-        public void AfficherResultat()
-        {
-            if (_jeuPendu.TestWin())
-            {
-                Console.WriteLine("Félicitations ! Vous avez trouvé le mot.");
-            }
-            else
-            {
-                Console.WriteLine("Désolé, vous avez perdu. Le mot était : " + _jeuPendu.GetMotATrouver());
-            }
-        }
-
-        public void AfficherNombreEssais()
-        {
-            Console.WriteLine("Nombre d'essais restants: " + _jeuPendu.GetNombreEssais());
-        }
+    public void AfficherNombreEssais()
+    {
+        Console.WriteLine("Nombre d'essais restants: " + _jeuPendu.GetNombreEssais());
     }
 }
